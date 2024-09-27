6798910bfd459db61ecea8f1e60d277338bea970
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-09-26 20:42:56 -0600

    avances

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from cryptography.fernet import Fernet
+
 import datetime
 
 db = SQLAlchemy()
@@ -8,13 +10,16 @@ class User(db.Model):
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
-    salt = db.Column(db.String(32), nullable=False)  # Asegúrate de tener esta línea
+    salt = db.Column(db.String(32), nullable=False) 
+    encryption_key = db.Column(db.String(128), nullable=False)
+    state = db.Column(db.Boolean, default=False)
 
     def __init__(self, username, email, password_hash, salt):
         self.username = username
         self.email = email
         self.password_hash = password_hash
         self.salt = salt
+        self.encryption_key = Fernet.generate_key().decode('utf-8')
         
 
 
