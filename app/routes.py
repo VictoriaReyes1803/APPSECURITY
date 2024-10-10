f03bc31aa0ac7fdf6a0ff125d510af2b64452e76
JonTheJol <cjcb040404@hotmail.com>
2024-10-10 13:39:22 -0600

    otra correcion a put

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ routes = Blueprint('routes', __name__)
 routes.add_url_rule('/api/register', view_func=AuthController.register, methods=['POST'])
 routes.add_url_rule('/api/verify/<token>', view_func=AuthController.verify, methods=['GET'])
 routes.add_url_rule('/api/login', view_func=AuthController.login, methods=['POST'])
-routes.add_url_rule('/api/logout', view_func=AuthController.logout, methods=['POST'])  # Changed to POST
+routes.add_url_rule('/api/logout', view_func=AuthController.logout, methods=['PUT'])
 routes.add_url_rule('/protected', view_func=AuthController.protected, methods=['GET'])
 routes.add_url_rule('/api/user', view_func=AuthController.me, methods=['GET'])
 
